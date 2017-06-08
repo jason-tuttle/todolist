213c85de33181d1268d7bb897aca4e7ba95a248c
Jason Tuttle <jason@hearspotbark.com>
2017-06-08 14:22:51 -0400

    main.js done

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 // Add your JS here.
+var newToDo = document.querySelector('.new-todo');
+var toDoList = document.querySelector('.todo-list');
+
+
+ newToDo.addEventListener("keypress", function(event) {
+   if (event.keyCode === 13) {
+     let newItem = event.target.value;
+     event.target.value = "";
+     var newToDoItem = document.createElement('li');
+     newToDoItem.innerHTML = '<li><div class="view"><label>' + newItem + '</label></div></li>';
+     toDoList.appendChild(newToDoItem);
+   }
+   return event;
+ });
